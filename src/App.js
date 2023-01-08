854a00c8252fdf7f19dfef1d62b22c2c1ceace0e
mcquam <murray.mcquarrie@gmail.com>
2023-01-08 18:26:35 +0000

    stuff done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>Personal Digeital stuff</div>
+            <div className="container">
+                <section className="section">
+                    <div classname="columns">
+                        <div classname="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+                        </div>
+                        <div classname="column is-4">
+                            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+                        </div>
+                        <div classname="column is-4">
+                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
 }
 
 export default App;
